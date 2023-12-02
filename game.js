0ed93820003081a65cf7ffb68eb8f4ceb9435da2
Israel Rotimi <izzyrotimi@gmail.com>
2023-12-02 14:29:01 +0000

    little update

--- game.js
+++ game.js
@@ -7,3 +7,31 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 700;
 canvas.height = 400;
+
+let gameSpeed = 5;
+
+
+
+//Background
+class Layer {
+    constructor(image, speedModifier){
+        this.x = 0;
+        this.y = 0;
+        this.width = 700;
+        this.height = 400;
+        this.image = image;
+        this.speedModifier = speedModifier;
+        this.speed = gameSpeed * this.speedModifier;
+    }
+    update(){
+        this.speed = gameSpeed * this.speedModifier;
+        if(this.x <= -this.width){
+            this.x = 0;
+        }
+        this.x = Math.floor(this.x - this.speed);
+    }
+    draw(){
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+        ctx.drawImage(this.image, this.x + this.width, this.y, this.width, this.height);
+    }
+}
