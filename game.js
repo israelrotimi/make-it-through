bb5bd6790cb0224c99a978f5e9b9b58d61794c71
Israel Rotimi <izzyrotimi@gmail.com>
2023-12-02 12:41:48 +0000

    Barebones setup

---
+++ game.js
@@ -0,0 +1,9 @@
+/**
+ * Make it through
+ * @type {HTMLCanvasElement}
+ */
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = 700;
+canvas.height = 400;
